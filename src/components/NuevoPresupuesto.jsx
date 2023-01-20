50d5261aa7a0b128a2acea4c1e349ab4f2cab687
Smerlyn García <smerlyngarcia@presidencia.gob.do>
2023-01-20 15:19:45 +0800

    Crando el formulario de presupuesto

---
+++ src/components/NuevoPresupuesto.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+
+const NuevoPresupuesto = ({presupuesto, setPresupuesto}) => {
+    return (
+        <div className='contenedor-presupuesto contenedor sombra'>
+            <form className='formulario'>
+                <div className='campo'>
+                    <label> Definir Presupuesto </label>
+                    <input
+                        className='nuevo-presupuesto'
+                        type="number"
+                        placeholder="Añade tu presupuesto"
+                        value={presupuesto}
+                        onChange={e => setPresupuesto(e.target.value)}
+                    />
+                    <input type="submit" value="Añadir"/>
+                </div>
+            </form>
+        </div>
+    )
+}
+
+export default NuevoPresupuesto
