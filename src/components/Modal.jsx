10c8c6760fe389a7512bc73f17f76ffcd3b40859
Smerlyn García <smerlyngarcia@presidencia.gob.do>
2023-01-23 10:54:36 +0800

    Agregando el fomulario de gastos

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -23,6 +23,45 @@ const Modal = ({ setModal, animarModal, setAnimarModal }) => {
             </div>
             <form className={`formulario ${animarModal ? 'animar' : 'cerrar'}`}>
                 <legend>Nuevo gasto</legend>
+
+                <div className='campo'>
+                    <label htmlFor='nombre'>Nombre gasto</label>
+
+                    <input
+                        id='nombre'
+                        type='text'
+                        placeholder='Añade el nombre del gasto'
+                    />
+                </div>
+                <div className='campo'>
+                    <label htmlFor='cantidad'>Cantidad</label>
+
+                    <input
+                        id='cantidad'
+                        type='number'
+                        placeholder='Añade la cantidad'
+                    />
+                </div>
+                <div className='campo'>
+                    <label htmlFor='categoria'>Nombre gasto</label>
+
+                    <select
+                        id='categoria'
+                    >
+                        <option value="">-- Seleccione --</option>
+                        <option value="ahorros">Ahorros</option>
+                        <option value="comida">Comida</option>
+                        <option value="casa">Casa</option>
+                        <option value="gastos">Gastos varios</option>
+                        <option value="ocio">Ocio</option>
+                        <option value="salud">Salud</option>
+                        <option value="suscripciones">Suscripciones</option>
+                    </select>
+                </div>
+                <input
+                    type="submit"
+                    value="Añadir gasto"
+                />
             </form>
         </div>
     )
