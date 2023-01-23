2e8a29f241cc36b8982a3cdacd9d3b69050d3cce
Smerlyn García <smerlyngarcia@presidencia.gob.do>
2023-01-23 09:42:45 +0800

    Primeros pasos con el componente de control presupuesto

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import ControlPresupuesto from './ControlPresupuesto'
 import NuevoPresupuesto from './NuevoPresupuesto'
 
 
@@ -14,7 +15,9 @@ const Header = ({
 
             {isValidadPresupuesto ?
                 (
-                    <p>Control Presupuesto</p>
+                    <ControlPresupuesto
+                        presupuesto={presupuesto}
+                    />
                 ) :
                 (
                     <NuevoPresupuesto
