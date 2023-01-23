a8f18325d2f427acc2b35ad03c07199ed12067d2
Smerlyn García <smerlyngarcia@presidencia.gob.do>
2023-01-23 10:03:56 +0800

    Iniciando la creación del modal de gastos

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
 
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0);
   const [isValidadPresupuesto, setIsValidPresupuesto] = useState(false)
+
+  const [modal, setModal] = useState(false);
+
+  const handleNuevoGasto = () => {
+    setModal(true);
+  }
 
   return (
     <>
@@ -16,7 +23,17 @@ function App() {
           setIsValidPresupuesto={setIsValidPresupuesto}
         />
       </div>
+      {isValidadPresupuesto && (
+        <div className='nuevo-gasto'>
+          <img
+            src={IconoNuevoGasto}
+            alt='Icono nuevo gasto'
+            onClick={handleNuevoGasto}
+          />
+        </div>
+      )}
 
+      {modal && <p>Desde el modal</p>}
     </>
   )
 }
