ccfda6d113c38c205f9f91e96b2eee0899103954
Smerlyn García <smerlyngarcia@presidencia.gob.do>
2023-01-26 09:29:10 +0800

    Colocando presupuesto en localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,9 @@ function App() {
 
   const [gastos, setGastos] = useState([]);
 
-  const [presupuesto, setPresupuesto] = useState(0);
+  const [presupuesto, setPresupuesto] = useState(
+    Number(localStorage.getItem('presupuesto')) ?? 0
+  );
   const [isValidadPresupuesto, setIsValidPresupuesto] = useState(false)
 
   const [modal, setModal] = useState(false);
@@ -28,6 +30,18 @@ function App() {
 
     //console.log('Componente listo');
   }, [gastoEditar])
+
+  useEffect(() => {
+    localStorage.setItem('presupuesto', presupuesto ?? 0)
+  }, [presupuesto]);
+
+  useEffect(() => {
+    const presupuestoLS = Number(localStorage.getItem('presupuesto')) ?? 0;
+
+    if (presupuestoLS > 0) {
+      setIsValidPresupuesto(true)
+    }
+  })
 
   const handleNuevoGasto = () => {
     setModal(true);
